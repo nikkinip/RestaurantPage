73a37b80728cca18574fe499059b74a90fd9ad85
superdoggo83 <bramvandersluis83@gmail.com>
2024-06-07 09:33:04 +0200

    Added menu view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace RestaurantPage.Controllers
         {
             return View();
         }
+        public IActionResult Menu()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
