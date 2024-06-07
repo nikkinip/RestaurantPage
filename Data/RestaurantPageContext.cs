f8555a79640565d656c33a9111dcc4b7f4ab5051
superdoggo83 <bramvandersluis83@gmail.com>
2024-06-07 09:23:12 +0200

    Added model

---
+++ Data/RestaurantPageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RestaurantPage.Models;
+
+namespace RestaurantPage.Data
+{
+    public class RestaurantPageContext : DbContext
+    {
+        public RestaurantPageContext (DbContextOptions<RestaurantPageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RestaurantPage.Models.Restaurant> Restaurant { get; set; } = default!;
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantPage.Models
+{
+    public class Restaurant
+    { 
+        public int Id { get; set; }
+        public string? Dish { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public string? Origin { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+    
